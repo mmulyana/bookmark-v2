bd499a7c8aa5c928f5b9c6f2bbf3664adcdf0e91
mmulyana <mulyan.t20@gmail.com>
2023-11-15 20:22:45 +0700

    fix overflow

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -5,7 +5,7 @@ export default function Layout({ children }: React.PropsWithChildren) {
     <main>
       <Sidebar />
       <div className='ml-[240px] py-4 px-2 min-h-screen'>
-        <div className='bg-white rounded-2xl p-5 h-[calc(100vh-32px)]'>{children}</div>
+        <div className='bg-white rounded-2xl p-5 h-[calc(100vh-32px)] overflow-y-auto'>{children}</div>
       </div>
     </main>
   )
